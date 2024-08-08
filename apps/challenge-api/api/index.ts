be9d10759741d536ea0c234eeba11e9fd9c3bb38
Ricardo Domingues <rfdomingues98@gmail.com>
2024-08-08 13:18:27 +0100

    Changed api to not use vercel, use nodejs instead

--- apps/challenge-api/api/index.ts
+++ apps/challenge-api/api/index.ts
@@ -1,5 +1,6 @@
 import type { PageConfig } from "next";
-import { handle } from "@hono/node-server/vercel";
+import { serve } from "@hono/node-server";
+import Dockerode from "dockerode";
 import { Hono } from "hono";
 
 export const config: PageConfig = {
@@ -9,11 +10,13 @@ export const config: PageConfig = {
 };
 
 const app = new Hono().basePath("/api");
+const docker = new Dockerode();
 
 app.get("/hello", (c) => {
+  docker.createContainer({ Image: "hello-world" });
   return c.json({
     message: "Hello from Hono!",
   });
 });
 
-export default handle(app);
+serve(app);
